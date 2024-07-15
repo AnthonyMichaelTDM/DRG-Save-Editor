222555ae1299a1d0086b58e0d03304a972ac7926
simon-wg <simon.green@live.se>
2024-07-15 13:52:11 +0200

    Fixed a bug where season data didn't reset

--- src/main/python/components/StateManager.py
+++ src/main/python/components/StateManager.py
@@ -1,19 +1,52 @@
 from copy import deepcopy
 import struct
+from typing import Literal
 
-from definitions import RESOURCE_GUIDS
+from definitions import RESOURCE_GUIDS, SEASON_GUIDS
 from .Enums import Dwarf, Resource
 
 
 class Stats:
     dwarf_xp: dict[Dwarf, int]
     dwarf_promo: dict[Dwarf, int]
+    season_data: dict[int, dict[Literal["xp", "scrip"], int]] = dict()
     resources: dict[Resource, int] = dict()
     credits: int
     perk_points: int
     weapons: dict[int, list[int | bool]] = dict()
 
-    def get_dwarf_xp(save_bytes) -> None:
+    @staticmethod
+    def get_season_data(
+        save_bytes: bytes, season_guid: str
+    ) -> dict[Literal["xp", "scrip"], int]:
+        # scrip_marker = bytes.fromhex("546F6B656E73")
+        season_marker: bytes = bytes.fromhex(season_guid)
+        season_marker_pos: int = save_bytes.find(season_marker)
+        # season data does not exist
+        if season_marker_pos == -1:
+            raise ValueError("Season missing")
+
+        season_xp_offset = 169
+        scrip_offset = 209
+
+        season_xp_pos = save_bytes.find(season_marker) + season_xp_offset
+        scrip_pos = save_bytes.find(season_marker) + scrip_offset
+
+        season_xp = struct.unpack("i", save_bytes[season_xp_pos : season_xp_pos + 4])[0]
+        scrip = struct.unpack("i", save_bytes[scrip_pos : scrip_pos + 4])[0]
+
+        return {"xp": season_xp, "scrip": scrip}
+
+    @staticmethod
+    def get_seasons(save_bytes: bytes) -> None:
+        for season, guid in SEASON_GUIDS.items():
+            try:
+                Stats.season_data[season] = Stats.get_season_data(save_bytes, guid)
+            except ValueError:
+                print(f"Season {season} missing")
+
+    @staticmethod
+    def get_dwarf_xp(save_bytes: bytes) -> None:
 
         en_marker = b"\x85\xEF\x62\x6C\x65\xF1\x02\x4A\x8D\xFE\xB5\xD0\xF3\x90\x9D\x2E\x03\x00\x00\x00\x58\x50"
         sc_marker = b"\x30\xD8\xEA\x17\xD8\xFB\xBA\x4C\x95\x30\x6D\xE9\x65\x5C\x2F\x8C\x03\x00\x00\x00\x58\x50"
@@ -71,13 +104,15 @@ class Stats:
             Dwarf.ENGINEER: eng_num_promo,
         }
 
-    def get_credits(save_bytes) -> int:
+    @staticmethod
+    def get_credits(save_bytes: bytes) -> int:
         marker = b"Credits"
         offset = 33
         pos = save_bytes.find(marker) + offset
         return struct.unpack("i", save_bytes[pos : pos + 4])[0]
 
-    def get_perk_points(save_bytes) -> int:
+    @staticmethod
+    def get_perk_points(save_bytes: bytes) -> int:
         marker = b"PerkPoints"
         offset = 36
         if save_bytes.find(marker) == -1:
@@ -86,10 +121,12 @@ class Stats:
             pos = save_bytes.find(marker) + offset
             return struct.unpack("i", save_bytes[pos : pos + 4])[0]
 
-    def get_misc(save_bytes) -> None:
+    @staticmethod
+    def get_misc(save_bytes: bytes) -> None:
         Stats.credits = Stats.get_credits(save_bytes)
         Stats.perk_points = Stats.get_perk_points(save_bytes)
 
+    @staticmethod
     def get_resources(save_bytes: bytes) -> None:
         # extracts the resource counts from the save file
         # print('getting resources')
@@ -110,7 +147,8 @@ class Stats:
             unp = struct.unpack("f", save_bytes[pos:end_pos])
             Stats.resources[k] = int(unp[0])  # save resource count
 
-    def get_weapons(save_data) -> None:
+    @staticmethod
+    def get_weapons(save_data: bytes) -> None:
         weapon = save_data.find(b"WeaponMaintenanceEntry") + 0x2C
 
         WEAPON_SIZE = 0xD9
@@ -141,3 +179,11 @@ class Stats:
 
             Stats.weapons[weapon] = [xp, level, levelup]
             weapon += WEAPON_SIZE
+
+    @staticmethod
+    def get_initial_stats(save_bytes: bytes) -> None:
+        Stats.get_dwarf_xp(save_bytes)
+        Stats.get_resources(save_bytes)
+        Stats.get_misc(save_bytes)
+        Stats.get_seasons(save_bytes)
+        Stats.get_weapons(save_bytes)
