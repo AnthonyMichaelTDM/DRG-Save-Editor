84c3b12457fe9856744365cea94cb189d01c0db2
wahlp <wahlp322@gmail.com>
2024-08-02 00:50:45 +0800

    add test

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -24,3 +24,29 @@ def test_load_save(qtbot, save_data):
     for _, element in controller.widget.__dict__.items():
         if isinstance(element, QLineEdit):
             assert element.text()
+
+
+def test_modify_overclocks(qtbot, save_data):
+    stats = Stats()
+    stats.parse_data(save_data)
+
+    widget = EditorUI()
+    controller = Controller(widget, stats)  # type: ignore[arg-type]
+    controller.setup(save_data)
+
+    assert controller.widget.unforged_list.count() == 18
+
+    # modify overclocks
+    controller.remove_all_ocs()
+    assert controller.widget.unforged_list.count() == 0
+
+    tree = controller.widget.overclock_tree
+    # navigate down one branch of the tree until there is a selectable item
+    parent = tree.topLevelItem(0)
+    while parent and parent.childCount() > 0:
+        parent = parent.child(0)
+    # select the item
+    tree.setCurrentItem(parent)
+
+    controller.add_cores()
+    assert controller.widget.unforged_list.count() == 1
